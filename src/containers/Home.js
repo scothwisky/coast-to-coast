f222f732e20e65331ed0e20003bee01cd3baa20c
daminduLiyanage <damindu7@gmail.com>
2019-12-02 02:04:48 -0500

    Hamburger menu added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,13 @@
 import React, { PureComponent } from 'react'
+import {  
+    Button, 
+    Container,
+    Jumbotron,   
+    Row, 
+    Col,
+    Card 
+  } from 'react-bootstrap';
+import YouTube from 'react-youtube';
 
 class Home extends PureComponent {
     constructor(props) {
@@ -10,7 +19,102 @@ class Home extends PureComponent {
     }
 
     render() {
-        return (<div></div>)
+
+        const opts = {
+            width: "100%",
+            height: "100%"
+          };
+
+        return (<Jumbotron>
+            
+            <Container>
+                  <Row>
+                  <Col className="d-flex justify-content-center align-items-center pb-3" sm={6}>
+                      <Card style={{ width: '18rem' }}>
+                        <div className="p-2">
+                          <YouTube
+                            videoId="Y21EGqRAwtc"
+                            opts={opts}
+                            onReady={()=>{}}
+                          />
+                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
+                        </div>
+                        <Card.Body>
+                          <Card.Title>Card Title</Card.Title>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                    <Col className="d-flex justify-content-center align-items-center pb-3" sm={6}>
+                      <Card style={{ width: '18rem' }}>
+                        <div className="p-2">
+                          <YouTube
+                            videoId="Y21EGqRAwtc"
+                            opts={opts}
+                            onReady={()=>{}}
+                          />
+                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
+                        </div>
+                        <Card.Body>
+                          <Card.Title>Card Title</Card.Title>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                    
+                    <Col className="d-flex justify-content-center align-items-center pb-3" sm={6}>
+                      <Card style={{ width: '18rem' }}>
+                        <div className="p-2">
+                          <YouTube
+                            videoId="Y21EGqRAwtc"
+                            opts={opts}
+                            onReady={()=>{}}
+                          />
+                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
+                        </div>
+                        <Card.Body>
+                          <Card.Title>Card Title</Card.Title>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                    <Col className="d-flex justify-content-center align-items-center pb-3" sm={6}>
+                      <Card style={{ width: '18rem' }}>
+                        <div className="p-2">
+                          <YouTube
+                            videoId="Y21EGqRAwtc"
+                            opts={opts}
+                            onReady={()=>{}}
+                          />
+                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
+                        </div>
+                        <Card.Body>
+                          <Card.Title>Card Title</Card.Title>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                          </Card.Text>
+                          <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                    
+                  </Row>
+                </Container>
+
+        </Jumbotron>)
     }
 }
 
--- src/routes/NavigationRouter.js
+++ src/routes/NavigationRouter.js
@@ -10,7 +10,10 @@ import YouTube from 'react-youtube';
 import Images from '../theme/Images';
 
 import { 
-  Navbar, 
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
   Nav, 
   Button, 
   Container, 
@@ -38,79 +41,41 @@ class NavigationRouter extends Component {
       };
 
 
-        return (<Router>
+        return (
+          <Router>
             <div>
-              <Navbar bg="dark" variant="dark">
+              <Navbar bg="dark" variant="dark" expand="lg">
                 <Navbar.Brand href="/">Coast to Coast</Navbar.Brand>
-                <Nav className="mr-auto">
-                  <Nav.Link href="/student-visa">Contact us</Nav.Link>
-                  {/* <Nav.Link href="/work-visa"></Nav.Link> */}
-                  <Nav.Link href="/pr-visa">Help</Nav.Link>
-                </Nav>
-                <Nav>
-                  <Nav.Link href="#sinhala">සිංහල</Nav.Link>
-                  <Nav.Link eventKey={2} href="#memes">
-                    English
-                  </Nav.Link>
-                </Nav>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                    <Nav.Link href="/student-visa">Contact us</Nav.Link>
+                    <Nav.Link href="/pr-visa">Help</Nav.Link>
+                  </Nav>
+                  <hr style={{backgroundColor:"grey"}}/>
+                  <Nav>   
+                    <Nav.Link href="#sinhala">සිංහල</Nav.Link>
+                    <Nav.Link eventKey={2} href="#memes">
+                      English
+                    </Nav.Link>
+                  </Nav> 
+                </Navbar.Collapse>
+
+
+
               </Navbar>
-              <Jumbotron>
-                <Container>
-                  <Row>
-                    <Col>
-                      <Card style={{ width: '18rem' }}>
-                        <div className="p-2">
-                          <YouTube
-                            videoId="Y21EGqRAwtc"
-                            opts={opts}
-                            onReady={()=>{}}
-                          />
-                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
-                        </div>
-                        <Card.Body>
-                          <Card.Title>Card Title</Card.Title>
-                          <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                          </Card.Text>
-                          <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                    <Col>
-                      <Card style={{ width: '18rem' }}>
-                        <div className="p-2">
-                          <YouTube
-                            videoId="Y21EGqRAwtc"
-                            opts={opts}
-                            onReady={()=>{}}
-                          />
-                          {/* <Card.Img variant="top" src={Images.logo} Fluid /> */}
-                        </div>
-                        <Card.Body>
-                          <Card.Title>Card Title</Card.Title>
-                          <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                          </Card.Text>
-                          <Button variant="primary">Go somewhere</Button>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  </Row>
-                </Container>
-              </Jumbotron>
+ 
               {/* A <Switch> looks through its children <Route>s and
                   renders the first one that matches the current URL. */}
               <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
                 <Route path="/about">
                   <About />
                 </Route>
                 <Route path="/users">
                   <Users />
-                </Route>
-                <Route path="/">
-                  <Home />
                 </Route>
               </Switch>
             </div>
